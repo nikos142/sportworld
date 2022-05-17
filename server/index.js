dd9e388c98503b1974ea3cc9d741aeaa531c8406
nikos142 <104628593+nikos142@users.noreply.github.com>
2022-05-17 13:51:36 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,17 +2,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const path = require("path");
+const database = require("./database.js")
+const {matchObject, transferObject}=require("./objects/matchObject")
 
-const mysql= require('mysql')
 
-const connection =  mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "",
-    database: "sportuniversedb"
-  });
-  
-  connection.connect()
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*")
@@ -25,58 +18,119 @@ app.use(function(req, res, next) {
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/premierleague/teams", (req, res) => {
-  let sql = "SELECT * FROM premierleague"
-  connection.query(sql, (err, rows, fields) => {
-      if (err) throw err  
-      res.send(rows)
-    })
- });
-
- app.get("/premierleague/teams/:id", (req, res) => {
-   var id=req.params.id
-  let sql = "SELECT name, town , owner, stadium ,fname , lname FROM premierleague inner join football_coaches on coach_id=football_coaches.id where premierleague.id="+id
-  connection.query(sql, (err, rows, fields) => {
-      if (err) throw err  
-      console.log(rows)
-      res.send(rows)
-    })
- });
-
-app.get("/laliga/teams", (req, res) => {
-  let sql = "SELECT * FROM laliga"
-  connection.query(sql, (err, rows, fields) => {
-        if (err) throw err  
-        res.send(rows)
-    })
-  });
-
-app.get("/bundesliga/teams", (req, res) => {
-  let sql = "SELECT * FROM bundesliga"
-  connection.query(sql, (err, rows, fields) =>
-   {
-      if (err) throw err  
-      res.send(rows)
+app.get("/:league", async (req, res) => {
+      var league=req.params.league
+      console.log(league+" teams requested")
+      try
+      {
+        var league_id =await database.getLeague(league)
+        var teams =await database.getLeagueTeams(league_id)
+      }
+        catch(error)
+        {
+          console.log(error)
+        }
+        res.send(teams)
   })
-});
 
-app.get("/serieA/teams", (req, res) => {
-  let sql = "SELECT * FROM seriea"
-  connection.query(sql, (err, rows, fields) =>
-   {
-      if (err) throw err  
-      res.send(rows)
-   })
-});
+  app.get("/matches/:id", async (req, res) => {
+    var id=req.params.id
+    try{
+     const matches= await  database.getTeamsMatches(id)
+     var tempmatches=[]
+     for(i in matches){
+       const obj =Object.create(matchObject)
+         obj.id=matches[i].id
+         obj.league= await database.getLeagueById(matches[i].league_id)
+         obj.home_team= await database.getTeamById(matches[i].home_team_id)
+         obj.away_team= await database.getTeamById(matches[i].away_team_id)
+         obj.done=matches[i].done
+         var date=new Date(matches[i].date)
+         obj.date = date.getDate() +"-"+date.getMonth()+"-"+date.getFullYear()
+         var minutes=date.getMinutes()
+         if(minutes<10){
+             minutes="0"+minutes
+         }
+         obj.time=date.getHours()+":"+minutes
+         if(matches[i].done==1)
+         {
+            var score  = await database.getMatchScore(matches[i].id)
+            obj.home_team_score = score[0].home_team_score
+            obj.away_team_score = score[0].away_team_score
+         }
+            tempmatches.push(obj)
+        }
+            res.send(tempmatches)
+          }
+          catch(error)
+          {
+            console.log(error)
+          }
+    })
 
-app.get("/ligue1/teams", (req, res) => {
-  let sql = "SELECT * FROM ligue1"
-  connection.query(sql, (err, rows, fields) =>
+  app.get("/profile/:id", async (req, res) => {
+        var id=req.params.id
+        try{
+              var profile= await database.getTeamsProfile(id);
+          }
+          catch(error)
+          {
+            console.log(error)
+          }
+          res.send(profile)
+    })
+
+    app.get("/players/:id" , async (req, res) => {
+      var id=req.params.id
+      try{
+        var players= await database.getTeamsRoster(id)
+          }
+          catch(error)
+          {
+            console.log(error)
+          }
+          res.send(players)
+     })
+     
+
+ app.get("/rules/:id", async (req, res) => {
+   var id=req.params.id
+   try{
+     var rules =await database.getRules(id)
+   }
+   catch
    {
-      if (err) throw err  
-      res.send(rows)
-   })
-});
+     console.log(error)
+   }
+   res.send(rules)
+ })
+ 
+ app.get("/transfers/:id", async (req, res) =>
+ {
+   var id=req.params.id
+   try{
+      var transfers =await database.getTransfers(id)
+      var data=[]
+     for(i in transfers)
+     {
+       const obj=new Object(transferObject)
+       obj.id=transfers[i].id
+       obj.fee=transfers[i].fee
+       var date=new Date(transfers[i].date)
+       obj.date = date.getDate() +"-"+date.getMonth()+"-"+date.getFullYear()
+       obj.player = await database.getPlayer(transfers[i].player_id)
+       obj.from = await database.getTeamById(transfers[i].from_team)
+       obj.to =await database.getTeamById(transfers[i].to_team)
+       console.log(obj)
+      data.push(obj)
+     }
+   }
+   catch(error)
+   {
+     console.log(error)
+   }
+   res.send(data)
+ })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ server/objects/matchObject.js
@@ -0,0 +1,22 @@
+const matchObject = {
+    id:"",
+    league:"",
+    home_team:"",
+    away_team:"",
+    done:"",
+   home_team_score:"",
+   away_team_score:"",
+   date:"",
+   time:""
+}
+
+
+const transferObject={
+    id:"",
+    player:"",
+    from:"",
+    to:"",
+    fee:"",
+    date:""
+}
+module.exports ={matchObject, transferObject}
